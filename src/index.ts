b35739eec408c61e41504ba7b58965062b5a8812
Zanardo <joaozzz0105@gmail.com>
2023-03-15 16:06:15 -0300

    change desktop path

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import BrokenDB1 from './data/broken/broken_database_1.json';
 import BrokenDB2 from './data/broken/broken_database_2.json';
 import { jsonToCsv, formatter, writer, join } from './helpers';
+import os from 'os';
 
 const { carsDB, brandsDB } = formatter(BrokenDB1, BrokenDB2);
 const finalDB = join(carsDB, brandsDB);
@@ -8,4 +9,4 @@ const finalDB = join(carsDB, brandsDB);
 writer(`${__dirname}/data/cars_database.json`, JSON.stringify(carsDB));
 writer(`${__dirname}/data/brands_database.json`, JSON.stringify(brandsDB));
 
-writer(`C:/Users/eders/Desktop/cars.csv`, jsonToCsv(finalDB));
+writer(`${os.homedir()}/Desktop/cars.csv`, jsonToCsv(finalDB));
